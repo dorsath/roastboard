c1a9d26fb86cef1f82cf8dacea4e04bd30e76fc0
Dorsath <ronald.m.hecker@gmail.com>
2016-12-20 20:39:50 +0100

    Add soundboard

---
+++ soundboard/index.js
@@ -0,0 +1,27 @@
+let sounds = require("./src/sounds.js");
+let play = function(file){
+  let a = new Audio('sounds/' + file);
+  a.currentTime = 0;
+  a.playbackRate = 1;
+  a.play();
+}
+
+
+
+let newButton = function(file, text){
+  let btn = document.createElement("div");
+  btn.appendChild(document.createTextNode(text));
+  btn.addEventListener("click", function(){
+    play(file);
+  });
+
+  return btn;
+}
+
+let container = document.getElementById("soundbuttons");
+
+
+for (var key in sounds){
+  container.appendChild(newButton(key, sounds[key]));
+  //console.log(key);
+}
