832f359fd483ab953ce14c5741fb074e18896eb3
Jorick van Hees <jorickvanhees@gmail.com>
2017-03-05 13:38:12 +0100

    Added serverside playing of audiofiles

--- soundboard/server/src/room.js
+++ soundboard/server/src/room.js
@@ -1,17 +1,32 @@
 var uuid = require('node-uuid');
+
+var path = require('path');
+var fs = require('fs');
+
 var Sounds = require('../../src/sounds.js');
 
 function Room(client, roomId){
   this.clients = [];
   this.uuid = roomId ? roomId : String(uuid.v4());
   this.addClient(client);
+  this.player = require('play-sound')(opts = {})
+  this.soundsFolder = path.resolve(appRoot, '..', 'public', 'sounds');
 }
 
 Room.prototype = {
   play: function(sound, sourceClient){
     console.log("playing ", sound, "in room", this.uuid);
+    var soundFile = path.resolve(this.soundsFolder, sound);
     if (Sounds[sound] == undefined)
       return;
+    // Play file on the server
+    if (fs.statSync(soundFile) && this.player){
+      this.player.play(soundFile, function(err){
+        if (err)
+          console.log('Couldn\'t play file "' + soundFile + '"');
+      })
+    }
+    // Send command to clients to play sound
     this.clients.forEach(function(client){
       if (client.uuid != sourceClient.uuid)
         client.send(this.playSoundCommand(sound));
